4b17c14082a754634da55c7e00728c5cd599b1cf
turbotecin <info.turbotec@gmail.com>
2022-08-23 23:12:15 +0530

    base_settings update

--- app/Base/Constants/Setting/Settings.php
+++ app/Base/Constants/Setting/Settings.php
@@ -104,7 +104,7 @@ class Settings
     const USER_CAN_MAKE_A_RIDE_AFTER_X_MINIUTES='user_can_make_a_ride_after_x_miniutes';
     const TRIP_ACCEPT_REJECT_DURATION_FOR_DRIVER='trip_accept_reject_duration_for_driver';
     const MAXIMUM_TIME_FOR_FIND_DRIVERS_FOR_REGULAR_RIDE='maximum_time_for_find_drivers_for_regular_ride';
-    const DEFAULT_COUNTRY_CODE_FOR_MOBILE_APP='default_country_code_for_mobile_app';
+    // const DEFAULT_COUNTRY_CODE_FOR_MOBILE_APP='default_country_code_for_mobile_app';
 
     
 }
